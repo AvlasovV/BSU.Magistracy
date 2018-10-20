64f0408e1e41a357507b48652b3cb141b56c8f12
Avlasov <barabasha999@mail.ru>
2018-10-20 17:29:26 +0300

    Long time without commits because I can't make my labs on work. So I don't need to bring changes in the cloud. I guess at this week I will fix this issue.

--- Algorithms/WebGraph/GeneratingWebGraph.py
+++ Algorithms/WebGraph/GeneratingWebGraph.py
@@ -3,21 +3,24 @@
 import networkx as nx
 import random as r
 from math import ceil
+from collections import defaultdict
+import pandas as ps
+import matplotlib.pyplot as plt
 
 
-def creating_web_graph(n, m, a=0.3):
+def creating_web_graph(n, m):
     g = nx.MultiGraph()
     # g.add_node(1)
     # g.add_edge(1,1)
-    for i in range(1, n * m + 1):
-        add_node_edge(g, i)
+    for value in range(1, n * m + 1):
+        add_node_edge(g, value)
     k = 0
-    for i in g.edges:
-        print(i, end="")
-        k += 1
-        if k == 10:
-            print()
-            k = 0
+    # for i in g.edges:
+    #     print(i, end="")
+    #     k += 1
+    #     if k == 10:
+    #         print()
+    #         k = 0
 
     g = transformation_to_web_graph(g, n, m)
     return g
@@ -41,7 +44,7 @@ def add_node_edge(graph, n):
 
 def transformation_to_web_graph(graph, n, m):
     """From fundamental graph function makes WemGraph by bollobash-riordan"""
-    g2 = nx.MultiDiGraph()
+    g2 = nx.MultiGraph()
     for i in range(1, n + 1):
         g2.add_node(i * m)
     for edge in graph.edges:
@@ -52,29 +55,59 @@ def transformation_to_web_graph(graph, n, m):
     return g2
 
 
+def draw_hist(g, m):
+    degree_dict = defaultdict(lambda: int(0))
+    for k in g.degree():
+        degree_dict[k[1]] += 1
+
+    ps.DataFrame(
+        {"Count of nodes": list(degree_dict.keys()), "Node degree": list(degree_dict.values())}).plot(
+        x="Count of nodes", kind="bar", color="red", figsize=(20, 12))
+    plt.savefig("Histogram 4k nodes m=" + str(m) + ".png")
+    plt.clf()
+
+
+def get_diam_avg_path_connected_components(g):
+    connected_components = list(nx.connected_component_subgraphs(g))
+    diam = max([nx.diameter(i) for i in connected_components])
+    count_of_nodes = []
+    for i in connected_components:
+        count_of_nodes.append(i.number_of_nodes())
+    average_short_path = sum(
+        nx.average_shortest_path_length(component) for component in connected_components) / len(
+        connected_components)
+    return diam, average_short_path, len(connected_components), count_of_nodes
+
+
 if __name__ == "__main__":
-    # for k in range(10):
-    #     g = generateNewNode(k)
-    #     print("Graph " + str(k) + ": ", g.nodes)
-    #     print(g.edges)
     import cProfile, pstats, io
     from pstats import SortKey
 
-    pr = cProfile.Profile()
-    pr.enable()
+    for i in range(10, 11):
 
-    g = creating_web_graph(4, 2)
-    # print("Graph " ": ", g.nodes)
-    print('\n', g.edges, end="\n")
-    print(g.nodes)
-    print("nodes_with_selfloops: ", [x for x in g.nodes_with_selfloops()], "\nnumber_of_selfloops^ ",
-          g.number_of_selfloops())
-    diam = nx.diameter(g)
-    average_short_path = nx.average_shortest_path_length(g)
+        """Here profiling code starts"""
+        pr = cProfile.Profile()
+        pr.enable()
 
-    pr.disable()
-    s = io.StringIO()
-    sortby = SortKey.CUMULATIVE
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())
+        g = creating_web_graph(100, i)
+
+        with open("Graph 4k, m=" + str(i) + ".txt", "w") as f:
+
+            diam, avg_shortest_path, count_of_components, count_of_nodes_in_component \
+                = get_diam_avg_path_connected_components(g)
+            f.write("Diameter in the biggest connected copmonent = " + str(diam) + '\n')
+            f.write("Average shortest path = " + str(avg_shortest_path) + '\n')
+            f.write("Count of connected components = " + str(count_of_components) + '\n')
+            f.write("Power of different connected components in Graph: \n")
+            for i in count_of_nodes_in_component:
+                f.write(str(i) + ", ")
+            f.write('\n')
+            draw_hist(g, i)
+
+            """ending profiling"""
+            pr.disable()
+            s = io.StringIO()
+            sortby = SortKey.CUMULATIVE
+            statistics = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            statistics.print_stats()
+            f.write(s.getvalue())
