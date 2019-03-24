727ef746074e99ce825449743a9bc3c00d9368d4
Avlasov <barabasha999@mail.ru>
2019-03-25 00:01:50 +0300

    Primitive root, work in progress

--- ProbabilisticAlgorithms/PrimitiveRoot/PrimitiveRoot.cpp
+++ ProbabilisticAlgorithms/PrimitiveRoot/PrimitiveRoot.cpp
@@ -34,12 +34,129 @@ roots.out
 
 using namespace std;
 
-int get_primitive_root(int prime){
-    int root = prime;
+// constant for wheel method
+long weel_spokes[] = { 1,2,2,4,2,4,2,4,6,2,6 };
 
+//long wheel(long n) {
+//	long ws[] = { 1,2,2,4,2,4,2,4,6,2,6 };
+//	long f = 2; int w = 0;
+//
+//	while (f * f <= n) {
+//		if (n % f == 0) {
+//			printf("%ld\n", f);
+//			n /= f;
+//		}
+//		else {
+//			f += ws[w];
+//			w = (w == 10) ? 3 : (w + 1);
+//		}
+//	}
+//	printf("%ld\n", n);
+//
+//	return 0;
+//}
 
-    return root;
+int gcd(int a, int b) {
+	while (b) {
+		a %= b;
+		swap(a, b);
+	}
+	return a;
 }
+
+//int powmod(int a, int b, int p) {
+//	int res = 1;
+//	while (b)
+//		if (b & 1)
+//			res = int(res * 1ll * a % p), --b;
+//		else
+//			a = int(a * 1ll * a % p), b >>= 1;
+//	return res;
+//}
+
+uint64_t mul_mod(uint64_t a, uint64_t b, uint64_t m)
+{
+	long double x;
+	uint64_t c;
+	int64_t r;
+	if (a >= m) a %= m;
+	if (b >= m) b %= m;
+	x = a;
+	c = x * b / m;
+	r = (int64_t)(a * b - c * m) % (int64_t)m;
+	return r < 0 ? r + m : r;
+}
+
+uint64_t pow_mod(uint64_t a, uint64_t b, uint64_t m)
+{
+	uint64_t r = m == 1 ? 0 : 1;
+	while (b > 0) {
+		if (b & 1)
+			r = mul_mod(r, a, m);
+		b = b >> 1;
+		a = mul_mod(a, a, m);
+	}
+	return r;
+}
+
+int get_primitive_root(uint64_t p) {
+	if (p == 2) {
+		return 1;
+	}
+	vector<uint64_t> factorization_vector;
+	uint64_t phi = p - 1;
+	uint64_t n = phi;
+	
+	if (n <= 1000000) {
+		// simple factorization -- O( sqrt(n) )
+		for (int i = 2; i*i <= n; ++i)
+			if (n % i == 0) {
+				factorization_vector.push_back(i);
+				while (n % i == 0)
+					n /= i;
+			}
+		if (n > 1)
+			factorization_vector.push_back(n);
+	}
+	else {
+		//	 wheel method
+		long f = 2; int w = 0;
+		while (f * f <= n) {
+			if (n % f == 0) {
+				// printf("%ld\n", f);
+				factorization_vector.push_back(f);
+				n /= f;
+			}
+			else {
+				f += weel_spokes[w];
+				w = (w == 10) ? 3 : (w + 1);
+			}
+		}
+		//printf("%ld\n", n);
+		if (n > 1)
+			factorization_vector.push_back(n);
+	}
+
+	/*for (int res = 2; res <= p; ++res) {
+		bool ok = true;
+	
+		for (size_t i = 0; i < factorization_vector.size() && ok; ++i)
+			ok &= pow_mod(res, phi / factorization_vector[i], p) != 1;
+		if (ok)  return res;
+	}*/
+	for (int k = 0; k < p; ++k) {
+		bool ok = true;
+		int res = 2 + rand() % (p - 2);
+		
+		for (size_t i = 0; i < factorization_vector.size() && ok; ++i) 
+			ok &= pow_mod(res, phi / factorization_vector[i], p) != 1;
+		
+		if (ok) return res;
+	}
+	return -1;
+}
+
+
 
 int main(){
     ifstream input("primes.in", std::ifstream::in);
@@ -52,9 +169,16 @@ int main(){
     for (int i = 0; i < n; i++){
         input >> prime;
         root = get_primitive_root(prime);
-        output << root << endl;
+		output << root << endl;
+
+		//cout << "Root = " << root << endl;
+		//cout << "Wheel\n";
+		//wheel(prime);
+		//cout << endl;
+
     }
 
     input.close();
     output.close();
+	//cin.get();
 }
